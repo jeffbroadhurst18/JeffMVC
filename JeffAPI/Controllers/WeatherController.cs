8155ffacff434466279c4cc9b699a711edf274b3
jeffbroadhurst18 <jeffbroadhurst18@outlook.com>
2019-12-05 21:22:50 +0000

    Changed to cope with just name as a parameter

--- JeffAPI/Controllers/WeatherController.cs
+++ JeffAPI/Controllers/WeatherController.cs
@@ -17,20 +17,32 @@ namespace JeffAPI.Controllers
     {
 		private IWeatherService _weatherService;
 		private readonly IMapper _mapper;
+		private List<WeatherParameters> _cities;
+
 		public WeatherController(IWeatherService weatherService, IMapper mapper)
 		{
 			_weatherService = weatherService;
 			_mapper = mapper;
+			_cities = GetCities();
 		}
 
 		// GET: api/Weather/5
 		[EnableCors("AnyGET")]
-		public async Task<ActionResult<JeffShared.ViewModel.Weather>> Get([FromQuery] WeatherParameters weatherParameters)
+		public async Task<ActionResult<JeffShared.ViewModel.Weather>> Get([FromQuery] WeatherParameters data)
 		{
+			var weatherParameters = _cities.FirstOrDefault(c => c.Name.ToLower() == data.Name.ToLower());
+
 			var weatherData = await _weatherService.GetForecast($"{ weatherParameters.Name},{weatherParameters.Country}");
 			var weather = _mapper.Map<JeffShared.ViewModel.Weather>(weatherData);
 			weather.Query = weatherParameters;
 			return weather;
+		}
+
+		[EnableCors("AnyGET")]
+		[Route("cities")]
+		public ActionResult<List<WeatherParameters>> Cities(bool uk)
+		{
+			return uk ? _cities.Where(c => c.Country == "uk").ToList() : _cities.Where(c => c.Country != "uk").ToList();
 		}
 
 		// POST: api/Weather
@@ -50,5 +62,28 @@ namespace JeffAPI.Controllers
         public void Delete(int id)
         {
         }
-    }
+
+		List<WeatherParameters> GetCities()
+		{
+			var cities = new List<WeatherParameters>();
+			cities.Add(new WeatherParameters { Name = "Northwich", Country = "uk", TimeLag = 0 });
+			cities.Add(new WeatherParameters { Name = "Durham", Country = "uk", TimeLag = 0 });
+			cities.Add(new WeatherParameters { Name = "London", Country = "uk", TimeLag = 0 });
+			cities.Add(new WeatherParameters { Name = "Bournemouth", Country = "uk", TimeLag = 0 });
+			cities.Add(new WeatherParameters { Name = "Edinburgh", Country = "uk", TimeLag = 0 });
+
+			cities.Add(new WeatherParameters { Name = "Stockholm", Country = "se", TimeLag = 1 });
+			cities.Add(new WeatherParameters { Name = "Copenhagen", Country = "dk", TimeLag = 1 });
+			cities.Add(new WeatherParameters { Name = "Roma", Country = "it", TimeLag = 1 });
+			cities.Add(new WeatherParameters { Name = "New", Country = "us", TimeLag = -5 });
+			cities.Add(new WeatherParameters { Name = "Paris", Country = "fr", TimeLag = 1 });
+			cities.Add(new WeatherParameters { Name = "Perth", Country = "au", TimeLag = 7 });
+
+			return cities;
+		}
+
+		
+	}
+
+	
 }
